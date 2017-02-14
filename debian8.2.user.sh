6808855b141cdd2969acaa2a7bed4a99bc5e4181
alexhokl <alexhokl@gmail.com>
2017-02-14 21:10:28 +0800

    Fixed typos

--- debian8.2.user.sh
+++ debian8.2.user.sh
@@ -6,8 +6,8 @@ curl https://raw.githubusercontent.com/alexhokl/installation/master/debian8.4.us
 chmod +x debian8.3.user.sh
 chmod +x debian8.4.user.sh
 
-curl -L https://github.com/git/git/archive/v.2.11.1.zip -o git.zip
+curl -L https://github.com/git/git/archive/v2.11.1.zip -o git.zip
 unzip git.zip
 cd git-*
 make prefix=/usr/local all
-sudo make prefix/usr/local
+sudo make prefix=/usr/local install
