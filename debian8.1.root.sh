a6c3935fd3b791c47c8f9f564a83b1b82f75977f
alexhokl <alexhokl@gmail.com>
2017-10-01 13:39:55 +0800

    Added mono repo

--- debian8.1.root.sh
+++ debian8.1.root.sh
@@ -21,6 +21,9 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 
+# mono
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+
 apt-get update
 apt-get -y upgrade
 
