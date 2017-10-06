f558a3f8b2364bdf9c122ac1b55b00502d8f07e4
alexhokl <alexhokl@gmail.com>
2017-10-06 16:13:43 +0800

    Updated git version

--- debian8.2.user.sh
+++ debian8.2.user.sh
@@ -6,8 +6,8 @@ curl https://raw.githubusercontent.com/alexhokl/installation/master/debian8.4.us
 chmod +x debian8.3.user.sh
 chmod +x debian8.4.user.sh
 
-curl -L https://github.com/git/git/archive/v2.11.1.zip -o git.zip
-unzip git.zip
+curl -L https://github.com/git/git/archive/v2.14.2.tar.gz -o git.tar.gz
+tar xvzf git.tar.gz
 cd git-*
 make prefix=/usr/local all
 sudo make prefix=/usr/local install
