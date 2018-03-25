c35541cb41f4ed7f604c9b498740d84a84535fc2
alexhokl <alexhokl@gmail.com>
2018-03-26 00:34:47 +0800

    Fixed Mono repo key

--- debian8.1.root.sh
+++ debian8.1.root.sh
@@ -22,7 +22,7 @@ curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microso
 mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 
 # mono
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 
 # virtual box
 curl https://www.virtualbox.org/download/oracle_vbox_2016.asc | apt-key add -
