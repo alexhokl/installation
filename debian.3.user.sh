89f3196f6629c2d3ad7356c887b7925c0b8e4a37
alexhokl <alexhokl@gmail.com>
2018-06-23 08:18:00 +0800

    Updated docker compose

--- debian.3.user.sh
+++ debian.3.user.sh
@@ -45,7 +45,7 @@ sudo dpkg -i keeweb.deb
 sudo apt-get -y install docker-ce
 sudo usermod -aG docker $USER
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.16.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 sudo curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o /etc/bash_completion.d/git-completion.bash
