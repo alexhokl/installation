b0b93a8c4579ef558f6fd65b0696b3a6b20c9a5c
alexhokl <alexhokl@gmail.com>
2018-11-29 21:35:53 +0800

    Removed Atom from Debian

--- debian.2.user.sh
+++ debian.2.user.sh
@@ -7,7 +7,6 @@ git clone https://github.com/rbenv/ruby-build.git ${HOME}/.rbenv/plugins/ruby-bu
 
 curl https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -o chrome.deb
 curl https://storage.googleapis.com/golang/go1.11.1.linux-amd64.tar.gz -o golang.tar.gz
-curl -L https://atom.io/download/deb -o atom.deb
 curl -sSL https://downloads.slack-edge.com/linux_releases/slack-desktop-3.2.1-amd64.deb -o slack.deb
 curl -sSL https://github.com/keeweb/keeweb/releases/download/v1.6.3/KeeWeb-1.6.3.linux.x64.deb -o keeweb.deb
 curl -sSL https://github.com/sharkdp/bat/releases/download/v0.6.1/bat-musl_0.6.1_amd64.deb -o bat.deb
@@ -40,7 +39,6 @@ go get github.com/kubernetes/kompose
 go get github.com/davecgh/go-spew/spew
 
 sudo dpkg -i chrome.deb
-sudo dpkg -i atom.deb
 sudo dpkg -i slack.deb
 sudo dpkg -i keeweb.deb
 sudo dpkg -i bat.deb
