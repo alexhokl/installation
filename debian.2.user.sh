7b681e540b6c460e75eac2c586c24895f6375663
alexhokl <alexhokl@gmail.com>
2018-12-02 05:16:20 +0000

    Added flutter to Debian

--- debian.2.user.sh
+++ debian.2.user.sh
@@ -10,6 +10,7 @@ curl https://storage.googleapis.com/golang/go1.11.1.linux-amd64.tar.gz -o golang
 curl -sSL https://downloads.slack-edge.com/linux_releases/slack-desktop-3.2.1-amd64.deb -o slack.deb
 curl -sSL https://github.com/keeweb/keeweb/releases/download/v1.6.3/KeeWeb-1.6.3.linux.x64.deb -o keeweb.deb
 curl -sSL https://github.com/sharkdp/bat/releases/download/v0.6.1/bat-musl_0.6.1_amd64.deb -o bat.deb
+curl https://storage.googleapis.com/flutter_infra/releases/beta/linux/flutter_linux_v0.11.13-beta.tar.xz -o ${HOME}/git/flutter.xz
 tar xvf golang.tar.gz
 source ${HOME}/git/dotfiles/.path
 
@@ -42,6 +43,8 @@ sudo dpkg -i chrome.deb
 sudo dpkg -i slack.deb
 sudo dpkg -i keeweb.deb
 sudo dpkg -i bat.deb
+
+tar xf ${HOME}/git/flutter.xz -C ${HOME}/git/
 
 sudo apt-get -y install docker-ce
 sudo usermod -aG docker $USER
