ee7c14b8a0c9bf2d9ee810ef2b7f190ed7076080
alexhokl <alexhokl@gmail.com>
2018-12-19 12:26:30 +0000

    Automated acceptance of EULA of Microsoft tools

--- debian.2.user.sh
+++ debian.2.user.sh
@@ -44,6 +44,8 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 sudo apt-get update
 sudo apt-get -y upgrade
 
+export ACCEPT_EULA=Y
+
 sudo apt-get install -y \
 		adduser \
 		alsa-utils \
