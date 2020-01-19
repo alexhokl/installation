76e81a759a3abd711e6ebe943dde4148c8f04611
alexhokl <alexhokl@gmail.com>
2020-01-19 21:31:48 +0800

    Updated scripts to dump software versions on GitHub

--- print-latest-versions.sh
+++ print-latest-versions.sh
@@ -2,16 +2,23 @@
 
 github-latest-release ()
 {
-    if [ -z "$1" ]; then
-        echo "No source repo specified";
-        return;
-    fi;
-    _status_code=$(curl -s -o /dev/null -I -w "%{http_code}" https://api.github.com/repos/${1}/releases/latest);
-    if [ "${_status_code}" = "404" ]; then
-        curl -s https://api.github.com/repos/${1}/releases | jq -r '.[0] | .tag_name';
-    else
-        curl -s https://api.github.com/repos/${1}/releases/latest | jq -r '.tag_name';
-    fi
+	if [ -z "$1" ]; then
+    echo "No source repo specified"
+    return
+	fi
+	_status_code=$(curl -s -o /dev/null -I -w "%{http_code}" https://api.github.com/repos/${1}/releases/latest)
+  if [ "${_status_code}" != "404" ]; then
+		curl -s https://api.github.com/repos/${1}/releases/latest | jq -r '.tag_name'
+		return
+	fi
+
+	_release_count=$(curl -s https://api.github.com/repos/${1}/releases | jq length)
+	if [ "$_release_count" != "0" ]; then
+		curl -s https://api.github.com/repos/${1}/releases | jq -r '.[0] | .tag_name'
+		return
+	fi
+
+  curl -s https://api.github.com/repos/${1}/tags | jq -r '.[0] | .name'
 }
 
 for i in $(grep VERSION_ versions-on-github); do
@@ -19,7 +26,9 @@ for i in $(grep VERSION_ versions-on-github); do
 	_r=${_s/VERSION_/}
 	_t=${_r/__/-}
 	_repo=${_t/_/\/}
-	_v=$(github-latest-release $_repo)
-	echo $_s=${_v/v/}
+	_v1=$(github-latest-release $_repo)
+	_v2=${_v1/_/.}
+	_version=${_v2/_/.}
+	echo $_s=${_version/v/}
 done
 
