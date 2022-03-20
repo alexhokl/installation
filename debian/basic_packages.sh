f90be8cd557fca71acebe91dc78a3c1ee345e0e9
alexhokl <alexhokl@gmail.com>
2022-03-20 13:25:17 +0800

    Added .NET 6 SDK to Debian

--- debian/basic_packages.sh
+++ debian/basic_packages.sh
@@ -22,6 +22,7 @@ sudo ACCEPT_EULA=Y DEBIAN_FRONTEND=noninteractive apt install -y \
 	dotnet-sdk-2.1 \
 	dotnet-sdk-3.1 \
 	dotnet-sdk-5.0 \
+	dotnet-sdk-6.0 \
 	exfat-fuse \
 	exfat-utils \
 	ffmpeg \
