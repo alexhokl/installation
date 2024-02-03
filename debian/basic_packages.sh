929ad1c944610ab1945b86ff0b125bdc7f244626
alexhokl <alexhokl@gmail.com>
2024-02-03 19:30:48 +0800

    Updated python3

--- debian/basic_packages.sh
+++ debian/basic_packages.sh
@@ -85,6 +85,9 @@ sudo ACCEPT_EULA=Y DEBIAN_FRONTEND=noninteractive apt install -y \
 	printer-driver-cups-pdf \
 	protonvpn \
 	python-dev \
+	python3 \
+	python3-dev \
+	python3-venv \
 	rclone \
 	sane \
 	sane-utils \
